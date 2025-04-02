bd32e3837d4e3deb445e6515793a252e4c09b352
André <andre.kuehnert@wandelbots.com>
2025-04-02 14:16:00 +0200

    feat: add infield correction models and update routes for camera verification and correction management

--- zivid_nova/routes/infield_correction.py
+++ zivid_nova/routes/infield_correction.py
@@ -1,12 +1,17 @@
 import uuid
+from typing import List
 
 import zivid
+import zivid.calibration
+import zivid.experimental.calibration
 from fastapi import APIRouter, HTTPException
 from loguru import logger
+from zivid.calibration import DetectionResult
+from zivid.experimental.calibration import InfieldCorrectionInput
 
 from zivid_nova import zivid_app
+from zivid_nova.models.infield_correction import AddCorrectionOffsetResp, CameraVerification
 
-# TODO https://support.zivid.com/en/latest/academy/camera/infield-correction/infield-correction-cli-tool.html
 router = APIRouter(prefix="/infield-correction", tags=["infield-correction"])
 
 correction_states = {}
@@ -14,7 +19,7 @@ correction_states = {}
 
 class Infield_Correction_State:
     def __init__(self, serial_number: str, correction_id: str):
-        self.camera_id = serial_number
+        self.serial_number = serial_number
         self.correction_id = correction_id
         self.dataset = []
 
@@ -25,13 +30,13 @@ async def read(serial_number: str) -> str:
     camera = zivid_app.get_connected_camera(serial_number)
     if zivid.experimental.calibration.has_camera_correction(camera):
         timestamp = zivid.experimental.calibration.camera_correction_timestamp(camera)
-        return {timestamp.strftime(r"%Y-%m-%d %H:%M:%S")}
+        return timestamp.strftime(r"%Y-%m-%d %H:%M:%S")
     else:
         raise HTTPException(status_code=404, detail="No infield correction found on camera.")
 
 
 @router.get("/verification")
-async def verify(serial_number: str):
+async def verify(serial_number: str) -> CameraVerification:
     """
     This function uses a single capture to determine the local dimension trueness error
     of the point cloud where the Zivid calibration board is placed.
@@ -50,7 +55,10 @@ async def verify(serial_number: str):
         f"Estimated dimension trueness error at measured position: {camera_verification.local_dimension_trueness() * 100:.3f}%"
     )
 
-    return camera_verification
+    pos = camera_verification.position()
+    logger.info(f"Position: {pos}")
+
+    return CameraVerification.from_zivid_camera_verification(camera_verification)
 
 
 @router.delete("")
@@ -63,21 +71,32 @@ async def reset(serial_number: str):
     zivid.experimental.calibration.reset_camera_correction(camera)
 
 
-@router.post("")
+@router.get("/correction")
+async def list_correction(serial_number: str) -> List[str]:
+    """
+    List all correction run IDs for the given serial number.
+    """
+    zivid_app.get_connected_camera(serial_number)
+    correction_ids = []
+    for correction_id in correction_states:
+        if correction_states[correction_id].serial_number == serial_number:
+            correction_ids.append(correction_id)
+    return correction_ids
+
+
+@router.post("/correction")
 async def start_correction(serial_number: str) -> str:
     """
     Will start a new correction run, by collection a dataset under the returned ID.
     """
-    # todo debatable
-    correction_states.clear()
-    camera = zivid_app.get_connected_camera(serial_number)
-    state = Infield_Correction_State(serial_number=camera.serial_number(), correction_id=str(uuid.uuid4()))
+    zivid_app.get_connected_camera(serial_number)
+    state = Infield_Correction_State(serial_number=serial_number, correction_id=str(uuid.uuid4()))
     correction_states[state.correction_id] = state
     return state.correction_id
 
 
-@router.post("/{correction_id}")
-async def add_correction_dataset(correction_id: str):
+@router.post("/correction/{correction_id}")
+async def add_correction_dataset(correction_id: str) -> AddCorrectionOffsetResp:
     """
     Add a new dataset to the correction run.
     """
@@ -91,10 +110,23 @@ async def add_correction_dataset(correction_id: str):
     verify_infield_input(infield_input)
 
     state.dataset.append(infield_input)
-    logger.info(f"Collected {len(state.dataset)} dataset for infield correction.")
+    logger.info(f"Collected {len(state.dataset)} datasets for infield correction.")
+
+    correction = zivid.experimental.calibration.compute_camera_correction(state.dataset)
+    accuracy_estimate = correction.accuracy_estimate()
+
+    result = AddCorrectionOffsetResp(
+        dimension_accuracy=accuracy_estimate.dimension_accuracy(),
+        dataset_size=len(state.dataset),
+        z_min=accuracy_estimate.z_min(),
+        z_max=accuracy_estimate.z_max(),
+    )
+
+    logger.info(f"Current estimated result {result}")
+    return result
 
 
-@router.put("/{correction_id}")
+@router.put("/correction/{correction_id}")
 async def write_correction_dataset(correction_id: str):
     """
     Calculates the correction based on the current dataset for the run. Clears the previous dataset.
@@ -104,16 +136,14 @@ async def write_correction_dataset(correction_id: str):
     correction = zivid.experimental.calibration.compute_camera_correction(state.dataset)
     accuracy_estimate = correction.accuracy_estimate()
     logger.info(
-        "This correction can be expected to yield a dimension accuracy error of",
-        f"{accuracy_estimate.dimension_accuracy() * 100:.3f}% or better in the range of z=[{accuracy_estimate.z_min():.3f}, {accuracy_estimate.z_max():.3f}] across the full FOV.",
-        "Accuracy close to where the correction data was collected is likely better.",
+        f"This correction can be expected to yield a dimension accuracy error of {accuracy_estimate.dimension_accuracy() * 100:.3f}% or better in the range of z=[{accuracy_estimate.z_min():.3f}, {accuracy_estimate.z_max():.3f}] across the full FOV. Accuracy close to where the correction data was collected is likely better.",
     )
     logger.info("Writing correction to camera...")
     zivid.experimental.calibration.write_camera_correction(camera, correction)
     del correction_states[correction_id]
 
 
-@router.delete("/{correction_id}")
+@router.delete("/correction/{correction_id}")
 async def delete_correction_dataset(correction_id: str):
     """ """
     if correction_id in correction_states:
@@ -126,7 +156,7 @@ def get_correction_state(correction_id: str) -> Infield_Correction_State:
     raise HTTPException(status_code=404, detail="Correction ID not found")
 
 
-def verify_infield_input(infield_input):
+def verify_infield_input(infield_input: InfieldCorrectionInput):
     """
     Verify if the infield input is valid for infield correction.
     """
@@ -137,7 +167,7 @@ def verify_infield_input(infield_input):
         )
 
 
-def verify_detection_result(detection_result):
+def verify_detection_result(detection_result: DetectionResult):
     if not detection_result.valid():
         raise HTTPException(
             status_code=404, detail=f"Calibration board not detected! Feedback: {detection_result.status_description()}"
