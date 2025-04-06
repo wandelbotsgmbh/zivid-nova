4c4fba37f4fdf400e29f4a1e5f315c1ec37cce3a
André <andre.kuehnie@gmail.com>
2025-04-06 20:30:54 +0200

    feat: add projector functionality (#12)
    * feat: add projector route and projection functionality
    
    * docs: update README with Pylint and Mypy instructions; clean up imports in projector.py
    
    * fix: simplify error message for calibration board detection
    
    * fix: streamline error handling for calibration board detection
    
    ---------
    
    Co-authored-by: André <andre.kuehnert@wandelbots.com>

---
+++ zivid_nova/routes/projector.py
@@ -0,0 +1,45 @@
+from typing import Dict
+
+import zivid
+from fastapi import APIRouter
+from loguru import logger
+from zivid.projection import ProjectedImage, projector_resolution, show_image_bgra
+
+from zivid_nova import zivid_app
+
+router = APIRouter(prefix="/projectors", tags=["projectors"])
+
+handles: Dict[str, ProjectedImage] = {}
+
+
+@router.post("/{serial_number}")
+async def project_test_image(serial_number: str):
+    """
+    Starts projection of a testimage for calibration board adjustment.
+    Stops the previous projection.
+    """
+    if serial_number in handles:
+        logger.info(f"stopping projector handle for {serial_number}...")
+        handles[serial_number].stop()
+
+    camera = zivid_app.get_connected_camera(serial_number)
+    resolution = projector_resolution(camera)
+    logger.info(f"Projector resolution: {resolution}")
+
+    # TODO support other resolutions as well
+    if resolution != (720, 1280):
+        logger.warning(f"Projector resolution is not 720x1280, but {resolution}")
+
+    image = zivid.Image.load("static/image.png", "rgba")
+    handles[serial_number] = show_image_bgra(camera, image.copy_data())
+
+
+@router.delete("/{serial_number}")
+async def delete_projection(serial_number: str):
+    """
+    Stops the projection for the given camera.
+    """
+    if serial_number not in handles:
+        return
+    logger.info(f"stopping projector handle for {serial_number}...")
+    handles[serial_number].stop()
